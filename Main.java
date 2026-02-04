01992e0546c10990a8b07013a0ef5a82a200f532
NikhilaVattikunta <vattikuntans@gmail.com>
2026-02-04 23:18:34 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+        int choice;
+
+        do {
+            System.out.println("\n1. Add Student");
+            System.out.println("2. Display Students");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    students.add(new Student(id, name));
+                    System.out.println("Student Added!");
+                    break;
+
+                case 2:
+                    if (students.isEmpty()) {
+                        System.out.println("No students available.");
+                    } else {
+                        for (Student s : students) {
+                            System.out.println(s);
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        } while (choice != 3);
+
+        sc.close();
+    }
+}
